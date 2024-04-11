80e2fa53d972c847f649adf3f88e04056c4eb666
Mirito10 <mathias.calderon@ulatina.net>
2024-04-11 16:13:00 -0600

    hola_mundo

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,1 +1,23 @@
 const express = require('express');
+const cors = require("cors");
+const app = express();
+
+
+app.use(cors());
+// Express middleware for parsing JSON
+app.use(express.json());
+
+
+
+
+const PORT = 3000;
+
+
+app.get('/hola_mundo', (req, res) => {
+  res.send('Esta es una prueba de respuesta del servidor, la respuesta es exitosa!');
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
